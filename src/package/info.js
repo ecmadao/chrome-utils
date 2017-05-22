b20e8526163c234d33e9d18cfa7124fb41e280f0
ecmadao <wlec@outlook.com>
2017-05-22 23:36:45 +0800

    WHAT: Make each extension id unique and identify
    WHY:
    
    * To identify each installed user
    
    HOW:
    
    * NOTHING TO SAY

--- src/package/info.js
+++ src/package/info.js
@@ -1,3 +1,6 @@
+import { uuid } from '../utils/helper';
+import store from './store';
+
 const getPlatform = (callback) => {
   chrome.runtime.getPlatformInfo((info) => {
     const platform = `${info.os}-${info.arch}`;
@@ -5,7 +8,18 @@ const getPlatform = (callback) => {
   });
 };
 
+const getIdentifyId = (callback) => {
+  store.get('identifyId', (result) => {
+    if (!result) {
+      result = uuid();
+      store.set('identifyId', result);
+    }
+    callback(result);
+  });
+};
+
 export default {
-  getId: () => chrome.runtime.id,
+  getUniqueId: () => chrome.runtime.id,
+  getIdentifyId,
   getPlatform
 };
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -125,3 +125,13 @@ export const createObj = (key, value) => {
   });
   return baseObj;
 };
+
+export const uuid = () => {
+  const s4 = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+};
